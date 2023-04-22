afbe81cd2ad91c4d607e647e0dde273e02446e71
Yuleavilez <123745874+DevJJMendez@users.noreply.github.com>
2023-04-22 09:12:43 -0500

    Front End, Ventana Inscripciones
    Modificación de la ventana inscripciones.

--- Presentacion/Presentacion/frmInscrip.Designer.cs
+++ Presentacion/Presentacion/frmInscrip.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInscrip));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,16 +44,20 @@
             this.tbxContacto = new System.Windows.Forms.TextBox();
             this.cbxProgramas = new System.Windows.Forms.ComboBox();
             this.pbxImagen = new System.Windows.Forms.PictureBox();
-            this.btnW = new System.Windows.Forms.Button();
-            this.btnX = new System.Windows.Forms.Button();
-            this.label9 = new System.Windows.Forms.Label();
+            this.ptbBackGround = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbBackGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 31);
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(65, 95);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 0;
@@ -61,7 +66,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(99, 69);
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(78, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 1;
@@ -70,7 +76,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(110, 110);
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(87, 201);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 2;
@@ -79,7 +86,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(96, 152);
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(73, 242);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 3;
@@ -88,7 +96,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(96, 194);
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(75, 287);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 4;
@@ -97,7 +106,8 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(35, 228);
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(12, 317);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(116, 13);
             this.label6.TabIndex = 5;
@@ -106,7 +116,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(217, 9);
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.Location = new System.Drawing.Point(150, 39);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(13, 13);
             this.label7.TabIndex = 6;
@@ -115,7 +126,9 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(133, 9);
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.Location = new System.Drawing.Point(52, 39);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(78, 13);
             this.label8.TabIndex = 7;
@@ -123,7 +136,7 @@
             // 
             // tbxID
             // 
-            this.tbxID.Location = new System.Drawing.Point(157, 31);
+            this.tbxID.Location = new System.Drawing.Point(136, 95);
             this.tbxID.Name = "tbxID";
             this.tbxID.Size = new System.Drawing.Size(121, 20);
             this.tbxID.TabIndex = 8;
@@ -131,28 +144,28 @@
             // 
             // tbxNombres
             // 
-            this.tbxNombres.Location = new System.Drawing.Point(157, 69);
+            this.tbxNombres.Location = new System.Drawing.Point(136, 145);
             this.tbxNombres.Name = "tbxNombres";
             this.tbxNombres.Size = new System.Drawing.Size(121, 20);
             this.tbxNombres.TabIndex = 9;
             // 
             // tbxCorreo
             // 
-            this.tbxCorreo.Location = new System.Drawing.Point(157, 110);
+            this.tbxCorreo.Location = new System.Drawing.Point(136, 198);
             this.tbxCorreo.Name = "tbxCorreo";
             this.tbxCorreo.Size = new System.Drawing.Size(121, 20);
             this.tbxCorreo.TabIndex = 10;
             // 
             // tbxDireccion
             // 
-            this.tbxDireccion.Location = new System.Drawing.Point(157, 152);
+            this.tbxDireccion.Location = new System.Drawing.Point(134, 242);
             this.tbxDireccion.Name = "tbxDireccion";
             this.tbxDireccion.Size = new System.Drawing.Size(121, 20);
             this.tbxDireccion.TabIndex = 11;
             // 
             // tbxContacto
             // 
-            this.tbxContacto.Location = new System.Drawing.Point(157, 194);
+            this.tbxContacto.Location = new System.Drawing.Point(134, 284);
             this.tbxContacto.Name = "tbxContacto";
             this.tbxContacto.Size = new System.Drawing.Size(121, 20);
             this.tbxContacto.TabIndex = 12;
@@ -160,55 +173,59 @@
             // cbxProgramas
             // 
             this.cbxProgramas.FormattingEnabled = true;
-            this.cbxProgramas.Location = new System.Drawing.Point(38, 246);
+            this.cbxProgramas.Location = new System.Drawing.Point(12, 347);
             this.cbxProgramas.Name = "cbxProgramas";
             this.cbxProgramas.Size = new System.Drawing.Size(240, 21);
             this.cbxProgramas.TabIndex = 13;
             // 
             // pbxImagen
             // 
-            this.pbxImagen.Location = new System.Drawing.Point(297, 31);
+            this.pbxImagen.Image = ((System.Drawing.Image)(resources.GetObject("pbxImagen.Image")));
+            this.pbxImagen.Location = new System.Drawing.Point(263, 95);
             this.pbxImagen.Name = "pbxImagen";
             this.pbxImagen.Size = new System.Drawing.Size(121, 141);
             this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImagen.TabIndex = 14;
             this.pbxImagen.TabStop = false;
             // 
-            // btnW
+            // ptbBackGround
             // 
-            this.btnW.Location = new System.Drawing.Point(102, 287);
-            this.btnW.Name = "btnW";
-            this.btnW.Size = new System.Drawing.Size(75, 23);
-            this.btnW.TabIndex = 15;
-            this.btnW.Text = "W";
-            this.btnW.UseVisualStyleBackColor = true;
+            this.ptbBackGround.Image = ((System.Drawing.Image)(resources.GetObject("ptbBackGround.Image")));
+            this.ptbBackGround.Location = new System.Drawing.Point(0, -1);
+            this.ptbBackGround.Name = "ptbBackGround";
+            this.ptbBackGround.Size = new System.Drawing.Size(800, 452);
+            this.ptbBackGround.TabIndex = 18;
+            this.ptbBackGround.TabStop = false;
             // 
-            // btnX
+            // pictureBox1
             // 
-            this.btnX.Location = new System.Drawing.Point(203, 287);
-            this.btnX.Name = "btnX";
-            this.btnX.Size = new System.Drawing.Size(75, 23);
-            this.btnX.TabIndex = 16;
-            this.btnX.Text = "X";
-            this.btnX.UseVisualStyleBackColor = true;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 374);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
             // 
-            // label9
+            // pictureBox2
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(217, 34);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(56, 13);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "<ENTER>";
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(99, 374);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
             // 
             // frmInscrip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.btnX);
-            this.Controls.Add(this.btnW);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pbxImagen);
             this.Controls.Add(this.cbxProgramas);
             this.Controls.Add(this.tbxContacto);
@@ -224,9 +241,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.ptbBackGround);
             this.Name = "frmInscrip";
             this.Text = "frmInscrip";
             ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbBackGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,8 +270,8 @@
         private System.Windows.Forms.TextBox tbxContacto;
         private System.Windows.Forms.ComboBox cbxProgramas;
         private System.Windows.Forms.PictureBox pbxImagen;
-        private System.Windows.Forms.Button btnW;
-        private System.Windows.Forms.Button btnX;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox ptbBackGround;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
