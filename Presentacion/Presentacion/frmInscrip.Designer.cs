6703714d302ee008369bb03ecff654693d1ad243
Yuleavilez <123745874+DevJJMendez@users.noreply.github.com>
2023-04-22 09:24:33 -0500

    Front End
    Modificaciones en la ventana Inscripciones.

--- Presentacion/Presentacion/frmInscrip.Designer.cs
+++ Presentacion/Presentacion/frmInscrip.Designer.cs
@@ -56,70 +56,77 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
-            this.label1.Location = new System.Drawing.Point(65, 95);
+            this.label1.Location = new System.Drawing.Point(150, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(119, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "Documento:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
-            this.label2.Location = new System.Drawing.Point(78, 145);
+            this.label2.Location = new System.Drawing.Point(172, 148);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(97, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombres:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
-            this.label3.Location = new System.Drawing.Point(87, 201);
+            this.label3.Location = new System.Drawing.Point(183, 201);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.Size = new System.Drawing.Size(86, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Correo:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
-            this.label4.Location = new System.Drawing.Point(73, 242);
+            this.label4.Location = new System.Drawing.Point(150, 245);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.Size = new System.Drawing.Size(119, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "Direccion:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
-            this.label5.Location = new System.Drawing.Point(75, 287);
+            this.label5.Location = new System.Drawing.Point(161, 284);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.Size = new System.Drawing.Size(108, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "Contacto:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
-            this.label6.Location = new System.Drawing.Point(12, 317);
+            this.label6.Location = new System.Drawing.Point(154, 330);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.Size = new System.Drawing.Size(240, 19);
             this.label6.TabIndex = 5;
             this.label6.Text = "Programa de formacion";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
-            this.label7.Location = new System.Drawing.Point(150, 39);
+            this.label7.Location = new System.Drawing.Point(374, 39);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.Size = new System.Drawing.Size(20, 19);
             this.label7.TabIndex = 6;
             this.label7.Text = "0";
             // 
@@ -127,61 +134,69 @@
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
-            this.label8.Location = new System.Drawing.Point(52, 39);
+            this.label8.Location = new System.Drawing.Point(172, 39);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.Size = new System.Drawing.Size(174, 19);
             this.label8.TabIndex = 7;
             this.label8.Text = "Inscripcion No.";
             // 
             // tbxID
             // 
-            this.tbxID.Location = new System.Drawing.Point(136, 95);
+            this.tbxID.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxID.Location = new System.Drawing.Point(275, 95);
             this.tbxID.Name = "tbxID";
-            this.tbxID.Size = new System.Drawing.Size(121, 20);
+            this.tbxID.Size = new System.Drawing.Size(121, 26);
             this.tbxID.TabIndex = 8;
             this.tbxID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxID_KeyPress);
             // 
             // tbxNombres
             // 
-            this.tbxNombres.Location = new System.Drawing.Point(136, 145);
+            this.tbxNombres.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxNombres.Location = new System.Drawing.Point(275, 145);
             this.tbxNombres.Name = "tbxNombres";
-            this.tbxNombres.Size = new System.Drawing.Size(121, 20);
+            this.tbxNombres.Size = new System.Drawing.Size(121, 26);
             this.tbxNombres.TabIndex = 9;
             // 
             // tbxCorreo
             // 
-            this.tbxCorreo.Location = new System.Drawing.Point(136, 198);
+            this.tbxCorreo.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxCorreo.Location = new System.Drawing.Point(275, 198);
             this.tbxCorreo.Name = "tbxCorreo";
-            this.tbxCorreo.Size = new System.Drawing.Size(121, 20);
+            this.tbxCorreo.Size = new System.Drawing.Size(121, 26);
             this.tbxCorreo.TabIndex = 10;
             // 
             // tbxDireccion
             // 
-            this.tbxDireccion.Location = new System.Drawing.Point(134, 242);
+            this.tbxDireccion.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxDireccion.Location = new System.Drawing.Point(273, 242);
             this.tbxDireccion.Name = "tbxDireccion";
-            this.tbxDireccion.Size = new System.Drawing.Size(121, 20);
+            this.tbxDireccion.Size = new System.Drawing.Size(121, 26);
             this.tbxDireccion.TabIndex = 11;
             // 
             // tbxContacto
             // 
-            this.tbxContacto.Location = new System.Drawing.Point(134, 284);
+            this.tbxContacto.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxContacto.Location = new System.Drawing.Point(273, 284);
             this.tbxContacto.Name = "tbxContacto";
-            this.tbxContacto.Size = new System.Drawing.Size(121, 20);
+            this.tbxContacto.Size = new System.Drawing.Size(121, 26);
             this.tbxContacto.TabIndex = 12;
             // 
             // cbxProgramas
             // 
+            this.cbxProgramas.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxProgramas.FormattingEnabled = true;
-            this.cbxProgramas.Location = new System.Drawing.Point(12, 347);
+            this.cbxProgramas.Location = new System.Drawing.Point(33, 352);
             this.cbxProgramas.Name = "cbxProgramas";
-            this.cbxProgramas.Size = new System.Drawing.Size(240, 21);
+            this.cbxProgramas.Size = new System.Drawing.Size(506, 27);
             this.cbxProgramas.TabIndex = 13;
             // 
             // pbxImagen
             // 
+            this.pbxImagen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pbxImagen.Image = ((System.Drawing.Image)(resources.GetObject("pbxImagen.Image")));
-            this.pbxImagen.Location = new System.Drawing.Point(263, 95);
+            this.pbxImagen.Location = new System.Drawing.Point(418, 98);
             this.pbxImagen.Name = "pbxImagen";
             this.pbxImagen.Size = new System.Drawing.Size(121, 141);
             this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -200,10 +215,11 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(15, 374);
+            this.pictureBox1.Location = new System.Drawing.Point(635, 380);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.Size = new System.Drawing.Size(66, 66);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 19;
             this.pictureBox1.TabStop = false;
@@ -211,10 +227,11 @@
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(99, 374);
+            this.pictureBox2.Location = new System.Drawing.Point(722, 380);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.Size = new System.Drawing.Size(66, 66);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox2.TabIndex = 20;
             this.pictureBox2.TabStop = false;
